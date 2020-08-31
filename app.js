c7c3f9ad6bf58037ed76076b0c997ff1fd56837d
Andrew Kilduff <a.l.kilduff@gmail.com>
2020-08-31 14:02:39 -0700

    Initialize React list renders

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-// TODO
+var TodoList = (props) => (
+  <ul>
+    <li>{props.todos[0]}</li>
+    <li>{props.todos[1]}</li>
+    <li>{props.todos[2]}</li>
+  </ul>
+);
+
+var App = () => (
+  <div>
+    <h2>My Todo List</h2>
+    <TodoList todos={['Learn React/Kale', 'Crush Recast.ly/Cucumbers', 'Maybe sleep/La Croix']}/> // Here we are creating an instance of the `TodoList` component
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));
